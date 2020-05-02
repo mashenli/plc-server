0f233600af0bf25f35afaab57b8f7cbcb6c20a46
sanfordma <oncwnuBR-ok9jJNFkj1nDKz6h7BQ@git.weixin.qq.com>
2020-05-02 20:02:40 +0800

    add: 添加login

--- routes/login.js
+++ routes/login.js
@@ -12,7 +12,25 @@ router.post('/login', function (req, res, next) {
 
 
   // res.render('login', {});
-  res.send('123')
+  let queryString = "select * from user where phoneNum='" + req.body.userName + "'";
+  db.query(queryString, function (err, rows) {
+    if (err) {
+      res.send(err);
+    } else {
+      if (rows.length == 0) {
+        res.send({ 'code': 0 });
+      } else if (rows[0].password == req.body.passWord) {
+        code = 1
+        let json = {}
+        json.data = rows[0]
+        json.code = code
+        res.send(json)
+      } else {
+        code = 2
+        res.send({ 'code': 2 });
+      }
+    }
+  })
 
 
 });
@@ -22,14 +40,14 @@ router.post('/login', function (req, res, next) {
 router.post('/admin/login', function (req, res, next) {
   console.log("登录接口");
   console.log(req.body)
-   //  0:不存在  1：登录成功   2：密码错误
+  //  0:不存在  1：登录成功   2：密码错误
   let queryString = "select * from admin where name='" + req.body.userName + "'";
   db.query(queryString, function (err, rows) {
     if (err) {
       res.send(err);
     } else {
       if (rows.length == 0) {
-        res.send({'code':0});
+        res.send({ 'code': 0 });
       } else if (rows[0].password == req.body.password) {
         code = 1
         let type = rows[0].identify
@@ -39,7 +57,7 @@ router.post('/admin/login', function (req, res, next) {
         res.send(json)
       } else {
         code = 2
-        res.send({'code':2});
+        res.send({ 'code': 2 });
       }
     }
   })
